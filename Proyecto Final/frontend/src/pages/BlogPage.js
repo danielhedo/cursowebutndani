4d7e37db1ab216a38b0c2983493c048bc2193338
Daniel Hedo <danielhedo@hotmail.com>
2023-04-02 12:35:44 +0200

    se añade la parte de blog

--- Proyecto Final/frontend/src/pages/BlogPage.js
+++ Proyecto Final/frontend/src/pages/BlogPage.js
@@ -4,7 +4,88 @@ const BlogPage = (props) =>
 {
 return(
 
-<div><p>hola</p></div>
+<div className="paginageneral contentText">
+      <h2>Blog</h2>
+      <div className="row">
+      <div className="col-2">
+        <a href="https://placeholder.com">
+          <img src="https://via.placeholder.com/100" alt="Blog post alt" />
+        </a>
+      </div>
+      <div className="col-9 tituloBlog" style={{ marginLeft: '3%' }}>
+        <h3>Cómo curar a una mascota</h3>
+        <h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h5>
+      </div>
+      <div className="row contentblog" style={{ marginTop: '1%' }}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          debitis deleniti! Aperiam illo nihil provident fuga fugiat dolorum
+          architecto enim perferendis reiciendis qui? Qui officia vitae soluta
+          veritatis tempore doloribus. Unde odit qui est. Dignissimos adipisci
+          totam rerum id neque a fugit maiores quo et iure veniam, unde esse
+          asperiores minima sequi optio cupiditate ipsam culpa quos alias vitae.
+          Repellat. Corrupti a rem ratione ipsum, amet expedita voluptatum? Nemo
+          ipsam quae tempore harum, ullam aut cumque ut expedita et pariatur
+          debitis atque eaque quod. Suscipit similique eaque consectetur harum
+          culpa.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          debitis deleniti! Aperiam illo nihil provident fuga fugiat dolorum
+          architecto enim perferendis reiciendis qui? Qui officia vitae soluta
+          veritatis tempore doloribus. Unde odit qui est. Dignissimos adipisci
+          totam rerum id neque a fugit maiores quo et iure veniam, unde esse
+          asperiores minima sequi optio cupiditate ipsam culpa quos alias vitae.
+          Repellat. Corrupti a rem ratione ipsum, amet expedita voluptatum? Nemo
+          ipsam quae tempore harum, ullam aut cumque ut expedita et pariatur
+          debitis atque eaque quod. Suscipit similique eaque consectetur harum
+          culpa.
+        </p>
+      </div>
+    </div>
+      
+      
+{/* Se añade otro post del blog para ver cómo queda */}
+
+<div className="row">
+      <div className="col-2">
+        <a href="https://placeholder.com">
+          <img src="https://via.placeholder.com/100" alt="Blog post alt" />
+        </a>
+      </div>
+      <div className="col-9 tituloBlog" style={{ marginLeft: '3%' }}>
+        <h3>Cómo curar a una mascota</h3>
+        <h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h5>
+      </div>
+      <div className="row contentblog" style={{ marginTop: '1%' }}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          debitis deleniti! Aperiam illo nihil provident fuga fugiat dolorum
+          architecto enim perferendis reiciendis qui? Qui officia vitae soluta
+          veritatis tempore doloribus. Unde odit qui est. Dignissimos adipisci
+          totam rerum id neque a fugit maiores quo et iure veniam, unde esse
+          asperiores minima sequi optio cupiditate ipsam culpa quos alias vitae.
+          Repellat. Corrupti a rem ratione ipsum, amet expedita voluptatum? Nemo
+          ipsam quae tempore harum, ullam aut cumque ut expedita et pariatur
+          debitis atque eaque quod. Suscipit similique eaque consectetur harum
+          culpa.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          debitis deleniti! Aperiam illo nihil provident fuga fugiat dolorum
+          architecto enim perferendis reiciendis qui? Qui officia vitae soluta
+          veritatis tempore doloribus. Unde odit qui est. Dignissimos adipisci
+          totam rerum id neque a fugit maiores quo et iure veniam, unde esse
+          asperiores minima sequi optio cupiditate ipsam culpa quos alias vitae.
+          Repellat. Corrupti a rem ratione ipsum, amet expedita voluptatum? Nemo
+          ipsam quae tempore harum, ullam aut cumque ut expedita et pariatur
+          debitis atque eaque quod. Suscipit similique eaque consectetur harum
+          culpa.
+        </p>
+      </div>
+    </div>
+
+</div>  
 
 );
 
