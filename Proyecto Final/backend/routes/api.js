a6e16f2bb0b41ddf42267267a61bc48898454eba
Daniel Hedo <danielhedo@hotmail.com>
2023-05-07 12:33:41 +0200

    se añade funcionalidad a contacto

--- Proyecto Final/backend/routes/api.js
+++ Proyecto Final/backend/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var blogModel = require('./../models/blogModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
+
 
 
 router.get('/blog', async function (req, res, next) {
@@ -31,4 +33,47 @@ router.get('/blog', async function (req, res, next) {
     });
     res.json(blog);
 });
+
+
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'daniel.veterinario@gmail.com',
+        subject: 'Contacto vía web',
+        html: `<head>
+        <meta charset="UTF-8">
+        <title>Información de contacto</title>
+      </head>
+      <body>
+        <h2>Información de contacto:</h2>
+        <p><strong>Nombre:</strong> ${req.body.nombre}</p>
+        <p><strong>Apellidos:</strong> ${req.body.apellidos}</p>
+        <p><strong>Email:</strong> ${req.body.email}</p>
+        <p><strong>Teléfono:</strong> ${req.body.telefono}</p>
+        <p><strong>Asunto:</strong> ${req.body.asunto}</p>
+        <p><strong>Mensaje:</strong> ${req.body.mensaje}</p>
+      </body>`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado... En breve nos pondremos en contacto con usted.'
+    });
+
+});
+
+
+
+
+
 module.exports = router;
